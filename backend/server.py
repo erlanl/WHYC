7ab03e855bd91541100dd697ba99eaf3be28e248
pedroalfonseca <paalf1337@gmail.com>
2023-08-28 16:17:09 -0300

    feat: adiciona lock para dicionário de salas ativas

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, session, jsonify
 from flask_socketio import SocketIO
 from flask_session import Session
 from datetime import datetime, timedelta
-from threading import Thread, Event
+from threading import Thread, Lock, Event
 from hashlib import sha256
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ Session(app)
 socketio = SocketIO(app)
 
 active_rooms = {}
+active_rooms_lock = Lock()
 
 SESSION_CLEANUP_INTERVAL = 30
 
@@ -28,20 +29,21 @@ def session_cleanup():
         now = datetime.now()
         empty_rooms = []
 
-        for hashed_room, session_data in active_rooms.items():
-            active_sessions = []
+        with active_rooms_lock:
+            for hashed_room, session_data in active_rooms.items():
+                active_sessions = []
 
-            for session_id, last_used in session_data:
-                if now - last_used <= app.permanent_session_lifetime:
-                    active_sessions.append((session_id, last_used))
+                for session_id, last_used in session_data:
+                    if now - last_used <= app.permanent_session_lifetime:
+                        active_sessions.append((session_id, last_used))
 
-            if active_sessions:
-                active_rooms[hashed_room] = active_sessions
-            else:
-                empty_rooms.append(hashed_room)
+                if active_sessions:
+                    active_rooms[hashed_room] = active_sessions
+                else:
+                    empty_rooms.append(hashed_room)
 
-        for hashed_room in empty_rooms:
-            active_rooms.pop(hashed_room)
+            for hashed_room in empty_rooms:
+                active_rooms.pop(hashed_room, None)
 
         Event().wait(SESSION_CLEANUP_INTERVAL)
 
@@ -50,10 +52,12 @@ def create_room():
     data = request.json
     hashed_room = compute_hashed_room(data["room"])
 
-    if hashed_room in active_rooms:
-        return jsonify({"message": "Room already exists."}), 400
+    with active_rooms_lock:
+        if hashed_room in active_rooms:
+            return jsonify({"message": "Room already exists."}), 400
 
-    active_rooms[hashed_room] = [(session.sid, datetime.now())]
+        active_rooms[hashed_room] = [(session.sid, datetime.now())]
+
     return jsonify({"message": "Room created successfully."}), 200
 
 @app.route("/join_room", methods=["POST"])
@@ -61,19 +65,21 @@ def join_room():
     data = request.json
     hashed_room = compute_hashed_room(data["room"])
 
-    if hashed_room not in active_rooms:
-        return jsonify({"message": "Room does not exist."}), 400
+    with active_rooms_lock:
+        if hashed_room not in active_rooms:
+            return jsonify({"message": "Room does not exist."}), 400
 
-    session_id = session.sid
-    active_sessions = active_rooms[hashed_room]
+        session_id = session.sid
+        active_sessions = active_rooms[hashed_room]
 
-    if any(session_info[0] == session_id for session_info in active_sessions):
-        return jsonify({"message": "Joined room successfully."}), 200
+        if any(session_info[0] == session_id for session_info in active_sessions):
+            return jsonify({"message": "Joined room successfully."}), 200
 
-    if len(active_rooms[hashed_room]) >= 2:
-        return jsonify({"message": "Room is full."}), 400
+        if len(active_rooms[hashed_room]) >= 2:
+            return jsonify({"message": "Room is full."}), 400
 
-    active_sessions.append((session_id, datetime.now()))
+        active_sessions.append((session_id, datetime.now()))
+
     return jsonify({"message": "Joined room successfully."}), 200
 
 @app.route("/leave_room", methods=["POST"])
@@ -81,16 +87,17 @@ def leave_room():
     data = request.json
     hashed_room = compute_hashed_room(data["room"])
 
-    if hashed_room not in active_rooms:
-        return jsonify({"message": "Room does not exist."}), 400
+    with active_rooms_lock:
+        if hashed_room not in active_rooms:
+            return jsonify({"message": "Room does not exist."}), 400
 
-    session_id = session.sid
-    active_sessions = active_rooms[hashed_room]
+        session_id = session.sid
+        active_sessions = active_rooms[hashed_room]
 
-    for session_info in active_sessions:
-        if session_info[0] == session_id:
-            active_sessions.remove(session_info)
-            return jsonify({"message": "Left room successfully."}), 200
+        for session_info in active_sessions:
+            if session_info[0] == session_id:
+                active_sessions.remove(session_info)
+                return jsonify({"message": "Left room successfully."}), 200
 
     return jsonify({"message": "You are not in this room."}), 400
 
