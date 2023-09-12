be75e8df1215d58d59741d43e3d31b1d4eb8f8d6
Amanda Quirino <aqrs@cin.ufpe.br>
2023-09-11 23:26:06 -0300

    fix: Deixando mais rápido a passagem das imagens

--- backend/api/controllers/game.py
+++ backend/api/controllers/game.py
@@ -2,6 +2,10 @@ from flask import request, session, jsonify
 from datetime import datetime, timedelta
 from hashlib import sha256
 from api.controllers.connectPlayers import *
+from PIL import Image, ImageFilter
+from urllib.request import urlretrieve
+from io import BytesIO
+import base64
 
 def run_game():
     start = datetime.now()
@@ -29,17 +33,34 @@ def get_answer(active_rooms):
 
     return jsonify({"message": "We cannot validate the word"}), 400
 
+def encodeProcess(img: Image):
+    buffer = BytesIO()
+    blimg = img.save(buffer, format='PNG')
+    blimg_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    return blimg_base64
+
 def get_image(active_rooms):
+    print("ENTREI")
     rejson = request.json
     id = rejson["id"]
     room = compute_hashed_room(rejson["room"])
     listPlayers = list(active_rooms[room].keys())
     nextPlayer = (listPlayers.index(id) + 1)%2
     nextPlayer = listPlayers[nextPlayer]
-    while "images" not in active_rooms[room][nextPlayer].keys():
+    while "imageURL" not in active_rooms[room][nextPlayer].keys():
         pass
-    images = active_rooms[room][nextPlayer]["images"]
+    url = active_rooms[room][nextPlayer]["imageURL"]
+    rd = 10
+    blurredImages = []
+    imgOr = Image.open(urlretrieve(url=url)[0])
+    blurredImages.append(encodeProcess(imgOr))
+
+    for i in range(1, 4):
+        print("Cheguei")
+        blimg = imgOr.filter(ImageFilter.BoxBlur(rd*i))
+        blurredImages.append(encodeProcess(blimg))
+        print("OLAA")
 
     return {
-        "data": images
+        "data": blurredImages
     }
