3871206bf19d7e578c3cbc192a9525af5f20a2f4
pedroalfonseca <paalf1337@gmail.com>
2023-09-13 12:40:51 -0300

    feat: adiciona animação de loading durante geração de imagem

--- frontend/src/components/screenGame/screenGenerate/generateImage.js
+++ frontend/src/components/screenGame/screenGenerate/generateImage.js
@@ -28,7 +28,7 @@ function Header() {
 
 function Main() {
     const [keyWordInput, setKeyWordInput] = useState(["", "", ""]);
-    const [imageURL, setImageURL] = useState(astroImage)
+    const [imageURL, setImageURL] = useState(null);
 
     return (
         <main className='flex flex-line items-center justify-center space-x-40'>
@@ -51,7 +51,7 @@ function Main() {
 
 function Image(props) {
     return (
-        <image className='pt-3'>
+        <image className='pt-3 relative'>
             <img src={props.url} alt='dalleImg' className='generateImg'/>
         </image>
     );
@@ -62,7 +62,7 @@ function KeyWord(props) {
         <keyword className='flex flex-line pb-3 justify-end items-center'>
             <num className='pr-3 text-2xl text-font-bege'>{props.numInput}.</num>
             <input type="text" name="inputKeyword" id="inputKeyword"
-                className="block rounded-md py-2 pr-20 sm:text-sm inputLabel"
+                className="block rounded-md py-5 pr-20 sm:text-lg inputLabel"
                 placeholder="Insira a palavra chave aqui"
                 value={props.input[props.numInput-1]}
                 onChange={(e) => props.setKeyWord(
@@ -92,7 +92,7 @@ function ChangePage(props) {
                     "room": codigo
                 });
         
-        if (res.status != 200) {
+        if (res.status !== 200) {
             passImg();
         }
     };
@@ -107,6 +107,8 @@ function ChangePage(props) {
 }
 
 function GenerateButton(props) {   
+    const [isLoading, setIsLoading] = useState(false);
+
     const generateImageClick = async () => {
         let codigo = sessionStorage.getItem("codigo")
         const id = sessionStorage.getItem("id")
@@ -116,6 +118,8 @@ function GenerateButton(props) {
         }
         else {
             try{
+                setIsLoading(true);
+
                 const res = await axios.post("http://localhost:5001/generate-image", {
                     "key_words": props.input,
                     "id": id,
@@ -138,12 +142,22 @@ function GenerateButton(props) {
                 console.error(err);
                 alert(err);
             }
+            finally {
+                setIsLoading(false);
+            }
         }
     }
 
     return(
         <generate>
-            <button className='button-home' onClick={generateImageClick}>GERAR</button>
+            {isLoading ? (
+                <svg aria-hidden="true" class="inline w-20 h-20 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-custom-color" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="rgba(251, 228, 216, 0.80)"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+            ) : (
+                <button className='button-home' onClick={generateImageClick}>GERAR</button>
+            )}
         </generate>
     );
 }
